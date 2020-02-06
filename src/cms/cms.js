8117b6dcf329ed48ce157a6e38aac77ed9adcf23
Kathleenmont <kathleen.m.montesano@gmail.com>
2020-02-06 15:17:57 -0500

    cloudinary

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,12 +2,22 @@ import React from 'react'
 import CMS from 'netlify-cms-app'
 import './cms-utils'
 
+
+// You only need to import the media library that you'll use. We register both
+// here for example purposes.
+import cloudinary from 'netlify-cms-media-library-cloudinary';
+
+
 import { HomePageTemplate } from '../templates/HomePage'
 import { ComponentsPageTemplate } from '../templates/ComponentsPage'
 import { ContactPageTemplate } from '../templates/ContactPage'
 import { DefaultPageTemplate } from '../templates/DefaultPage'
 import { BlogIndexTemplate } from '../templates/BlogIndex'
 import { SinglePostTemplate } from '../templates/SinglePost'
+
+
+CMS.registerMediaLibrary(cloudinary);
+
 
 if (
   window.location.hostname === 'localhost' &&
